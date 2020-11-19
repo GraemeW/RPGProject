80683e4d7dc84bb500532899bb4b98e976dd8cca
GraemeMW <graememwilliams@gmail.com>
2020-11-18 23:59:03 -0800

    Rebuild for 2019 and add simple navmesh agent control - my computer apparently hates 2018

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Mover : MonoBehaviour
+{
+    // Tunables
+    [SerializeField] Transform target = null;
+
+    // Cached References
+    NavMeshAgent navMeshAgent = null;
+
+    private void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        navMeshAgent.SetDestination(target.position);
+    }
+}

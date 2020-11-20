7d59fd8d73ed413e9025f694156e3ae7ad9ba052
GraemeMW <graememwilliams@gmail.com>
2020-11-19 23:04:24 -0800

    Point to click movement and main character model

---
+++ Assets/Scripts/Core/FollowCamera.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+    [SerializeField] Transform target = null;
+
+    private void LateUpdate()
+    {
+        transform.position = target.position;
+    }
+}
---
+++ Assets/Scripts/Movement/Mover.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Mover : MonoBehaviour
+{
+    // Tunables
+
+    // Cached References
+    NavMeshAgent navMeshAgent = null;
+    Animator animator = null;
+
+    // State
+    Ray lastRay;
+
+    private void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetButton("Fire1"))
+        {
+            MoveToCursor();
+        }
+        UpdateAnimator();
+    }
+
+    private void MoveToCursor()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        bool hasHit = Physics.Raycast(ray, out RaycastHit hitInfo);
+        if (hasHit)
+        {
+            navMeshAgent.SetDestination(hitInfo.point);
+        }
+        lastRay = ray;
+    }
+
+    private void UpdateAnimator()
+    {
+        Vector3 velocity = navMeshAgent.velocity;
+        Vector3 localVelocity = transform.InverseTransformDirection(velocity);
+        animator.SetFloat("forwardSpeed", localVelocity.z);
+    }
+}
